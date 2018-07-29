833269e732f044544cb981f4998433d56656ffa0
ajayns <ajay.ns@outlook.com>
2018-07-29 16:00:03 +0530

    :lipstick: Design starter example site

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,29 @@
 (function($) {
-  'use strict'; // Start of use strict
-  // Smooth scrolling using jQuery easing
+  "use strict"; // Start of use strict
 
+  // Shrink navbar on scroll
+  $(window).scroll(function(event) {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+
+  // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate(
+        $("html, body").animate(
           {
-            scrollTop: target.offset().top,
+            scrollTop: target.offset().top
           },
           1000
         );
@@ -26,16 +37,16 @@
   // var navBgColor = $("#mainNav").css("background-color", "#363f47");
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    if (window.innerWidth < '992') {
-      $('.navbar-collapse').collapse('hide');
-      $('#mainNav').css('background-color', '#111C33');
+  $(".js-scroll-trigger").click(function() {
+    if (window.innerWidth < "992") {
+      $(".navbar-collapse").collapse("hide");
+      $("#mainNav").css("background-color", "#111C33");
     }
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 57,
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 57
   });
 })(jQuery); // End of use strict
